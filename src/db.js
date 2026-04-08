77c9aa8751791f635a981d122231782aa88506c8
Monica Serrano <monicasesal@gmail.com>
2026-04-08 19:00:02 +0200

    cambio

--- src/db.js
+++ src/db.js
@@ -1,16 +1,19 @@
-//gestiona la conexión a la base de datos MySQL utilizando mysql2/promise
-
+// gestiona la conexión a la base de datos MySQL utilizando mysql2/promise
 const mysql = require('mysql2/promise')
 require('dotenv').config()
 
-const db = mysql.createPool({ //crea un pool de cnexiones a la base de datos, en vez de abrir y cerrar una conexión cada vez que haces una consulta
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true, //si se llenanm espera hasta que haya una libre
-    connectionLimit: 10, //hasta 10 conexiones simultáneas
-    queueLimit: 0 // sin limite de filas en espera
+    port: process.env.DB_PORT || 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0,
+    ssl: {
+        rejectUnauthorized: false   //Permite la conexión segura con Aiven
+    }
 })
 
-module.exports = db
+module.exports = db;
